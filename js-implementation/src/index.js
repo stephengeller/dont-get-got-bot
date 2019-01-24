fe2b42dc840ee04fb21abcc1323b2068a8dc4af3
Stephen Geller <stephen.gb.geller+github@gmail.com>
2019-01-24 16:10:38 +0000

    chore(dont-get-got-bot): Get "scores" working.

--- js-implementation/src/index.js
+++ js-implementation/src/index.js
@@ -1,33 +1,63 @@
 const querystring = require("querystring");
+const AWS = require("aws-sdk");
 
 if (process.env.ENVIRONMENT !== "prod") {
   require("dotenv").config();
+  AWS.config.credentials = new AWS.SharedIniFileCredentials({
+    profile: "personal"
+  });
 }
 
-const { REGION } = process.env;
-
-const AWS = require("aws-sdk");
+const { REGION, TABLE_NAME } = process.env;
 const docClient = new AWS.DynamoDB.DocumentClient({ region: REGION });
 
-const TABLE_NAME = "dont_get_got_scores";
+const getScores = () => {
+  const params = { TableName: TABLE_NAME };
+  return new Promise(async (resolve, reject) => {
+    await docClient.scan(params, (err, data) => {
+      if (err) {
+        reject(Error(err));
+      } else {
+        const { Items } = data;
+        resolve(Items);
+      }
+    });
+  });
+};
+
+const handleRequest = async request => {
+  switch (request.toLowerCase()) {
+    case "scores":
+      return await getScores()
+        .then(items => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify(items)
+          };
+        })
+        .catch(err => console.log(err));
+    default:
+      const res = {
+        statusCode: 400,
+        body: JSON.stringify(`${request} is not valid`)
+      };
+      console.log(res);
+      return res;
+  }
+};
 
 exports.handler = async event => {
   const params = querystring.parse(event.body);
-  let res = {
-    statusCode: null,
-    body: ""
-  };
   if (process.env.VERIFICATION_TOKEN !== params.token) {
-    res.statusCode = 401;
-    res.body = "Unauthorized!";
-    console.log(process.env.VERIFICATION_TOKEN);
-    console.log(params.token);
-  } else {
-    res = {
-      statusCode: 200,
-      body: JSON.stringify(params.text)
+    let res = {
+      statusCode: 401,
+      body: "Unauthorized"
     };
+    console.log(res);
+    return res;
+  } else {
+    res = await handleRequest(params.text);
+    console.log(res);
+    return res;
   }
-  console.log(res);
-  return res;
 };
