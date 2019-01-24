b4fb95c322f4e7aec8acdc10c8d5e071dcc1adc5
Stephen Geller <stephen.gb.geller+github@gmail.com>
2019-01-24 12:37:22 +0000

    feat(dont-get-got-bot): Create file to invoke lambda.

---
+++ invoke.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source ./.env
+
+OUTPUT_FILE=output.txt
+
+aws lambda invoke \
+    --function-name ${FUNCTION_ARN} \
+    --region ${REGION} \
+    --invocation-type RequestResponse \
+    --profile personal \
+    ./${OUTPUT_FILE}
+
+cat ${OUTPUT_FILE} && rm -rf ${OUTPUT_FILE}
